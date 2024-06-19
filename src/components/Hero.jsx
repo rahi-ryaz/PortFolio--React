158215b9eecdbfaf9d984d30ad466f8e2f66ceff
Rahi Reyaz <rahiryazdar@gmail.com>
2024-06-19 18:22:53 +0530

    Animated with framer motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,17 @@
 import  {HERO_CONTENT} from "../constants"
 import profilePic from  "../assets/Profile.png";
+import { motion } from "framer-motion"
+
+
+const Container = (delay) => ({
+   hidden:{x:-100, opacity:0},
+   visible:{
+      x:0,
+      opacity:1,
+      transition:{duration:0.5 , delay:delay},
+   },
+
+});
 
 const Hero = () => {
   return (
@@ -7,19 +19,38 @@ const Hero = () => {
        <div className = "flex flex-wrap">
              <div className ="w-full lg:w-1/2">
                 <div className ="flex flex-col items-center lg:items-start">
-                   <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16
-                   lg:text-8xl">Rahi Reyaz</h1>
+                   <motion.h1 
+                    variants={Container(0)}
+                    initial="hidden"
+                    animate="visible"
 
-                   <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500
-                   bg-clip-text text-3xl tracking-tight text-transparent">Full Stack Developer</span>
+                   className="pb-16 text-6xl font-thin tracking-tight lg:mt-16
+                   lg:text-8xl">Rahi Reyaz</motion.h1>
 
-                   <p className ="my-2 max-w-xl py-6 font-light tracking-tighter"> {HERO_CONTENT}</p>
+                   <motion.span
+                     variants={Container(0.5)}
+                     initial="hidden"
+                     animate="visible"
+                   className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500
+                   bg-clip-text text-3xl tracking-tight text-transparent">Full Stack Developer</motion.span>
+
+                   <motion.p
+                     variants={Container(1)}
+                     initial="hidden"
+                     animate="visible"
+                   className ="my-2 max-w-xl py-6 font-light tracking-tighter"> {HERO_CONTENT}
+                   </motion.p>
 
                  </div>
             </div> 
             <div className ="w-full lg:w-1/2 lg:p-8">
             <div className ="flex justify-center">
-                <img src={profilePic} alt ="Rahi Reyaz"></img>
+                <motion.img 
+                  initial={{x:100 ,opacity:0}}
+                  animate={{x:0 ,opacity:1}}
+                  transition={{duration:1, delay:1.2}}
+                src={profilePic} 
+                alt ="Rahi Reyaz"></motion.img>
             </div>
              </div>
         </div> 
