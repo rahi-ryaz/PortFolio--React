3541dc1123283d1e897243c42878ec26a67d4f45
Rahi Reyaz <rahiryazdar@gmail.com>
2024-06-19 23:31:41 +0530

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,36 +1,38 @@
-import aboutImg from "../assets/about.jpg"
-import { ABOUT_TEXT } from "../constants"
-import {motion} from "framer-motion";
+
+import { motion } from "framer-motion";
+import { ABOUT_TEXT } from "../constants";
+import aboutImg from "../assets/about.jpg";
 
 const About = () => {
   return (
-    <div className =" -mx-10 border-b border-neutral-900 ">
-                    
-                     <h1 className="my-20 text-center text-4xl">About 
-                            <span className="text-neutral-500"> Me</span>
-                      </h1>
+    <div className="px-4 md:px-10 border-b border-neutral-900">
+      <h1 className="my-10 md:my-20 text-center text-4xl">
+        About <span className="text-neutral-500">Me</span>
+      </h1>
 
-                  <div className ="flex flex-wrap ">
-                     
-                     <motion.div
-                     whileInView={{opacity:1 ,x:0}}
-                     initial={{opacity:0 ,x:-100}}
-                     transition={{duration:0.5}}
-                     className="w-full lg:w-1/2 lg:p-10 flex justify-center  ">
-                       <img className="rounded-2xl h-2/3"src={aboutImg} alt="about"/> 
-                     </motion.div>
+      <div className="flex flex-col-reverse md:flex-row items-center">
+        <motion.div
+          className="w-full md:w-1/2 lg:p-10 flex justify-center"
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+        >
+          <img className="rounded-2xl h-auto md:h-96" src={aboutImg} alt="about" />
+        </motion.div>
 
-
-                         <motion.div
-                          whileInView={{opacity:1 ,x:0}}
-                          initial={{opacity:0 ,x:100}}
-                          transition={{duration:0.5}}
-                         className ="w-full lg:w-1/2 lg:p-10 flex justify-center lg:justify-left">
-                                     <p className="text-justify my-2 max-w-xl py-6 font-light tracking-tighter">{ABOUT_TEXT}</p>
-                         </motion.div>
-                  </div>
+        <motion.div
+          className="w-full md:w-1/2 lg:p-10 flex justify-center md:justify-start"
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.5 }}
+        >
+          <p className="text-justify my-2 max-w-xl py-6 md:py-0 md:pl-10 font-light tracking-tighter">
+            {ABOUT_TEXT}
+          </p>
+        </motion.div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
